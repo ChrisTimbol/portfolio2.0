4a51a35df027924d3f09944dd2f5df2c71a24ef5
Chris Timbol <christimbol707@gmail.com>
2023-09-20 12:11:02 -0700

    made hamburger appear at top of backdrop menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,27 @@ import Hamburger from '@/components/Hamburger'
 import React, { useState } from 'react';
 import Link from 'next/link';
 export default function Navbar() {
-    const [hamburger, setHamburger] = useState(false)
-    const toggleHamburger = () => {
-        setHamburger(!hamburger) /* sets inverse  */
+    const [menu, setMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setMenu(!menu)
     }
 
     return (
-        <nav className=" ">
-            <button onClick={toggleHamburger}>
-            <Hamburger hamburger={hamburger}/>
-
-{/*             <div className='fixed inset-0  backdrop-blur	 overflow-hidden   bg-opacity-70 bg-gray-700'>
-                    <ul className='absolute left-0 bottom-0 p-2 h-5/6 w-full items-center justify-center flex flex-col justify-around'>
+        <nav className="">
+            <div className="relative z-10" onClick={toggleMenu} >
+                <Hamburger />
+            </div>
+            {menu && (
+                <div className='fixed inset-0  backdrop-blur	 overflow-hidden   bg-opacity-70 bg-gray-700'>
+                    <ul className={`absolute left-0 bottom-0 p-2 h-5/6  w-full justify-center flex flex-col justify-around `}>
                         <li className='text-5xl'><Link href="/">Home</Link></li>
                         <li className='text-5xl'><Link href="/blog">Blog</Link></li>
                         <li className='text-5xl '><Link href="/work">Work</Link></li>
                         <li className='text-5xl '><Link href="/about">About</Link></li>
                     </ul>
-                </div> */}
-
-            </button>
-           
+                </div>
+            )}
         </nav >
     )
 }
