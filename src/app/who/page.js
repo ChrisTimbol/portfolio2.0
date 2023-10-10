cbbf95a9507f6dc3e97c796d869215ff349ca838
Chris Timbol <christimbol707@gmail.com>
2023-10-10 12:52:33 -0700

    turn titles to slate from cyan

--- src/app/who/page.js
+++ src/app/who/page.js
@@ -5,13 +5,13 @@ import Link from 'next/link'
 export default function Who() {
     return (
         <>
-      <main className="flex justify-center  w-full h-full  bg-gradient-to-b from-slate-950 via-slate-900 to-slate-900 text-slate-50   ">
+            <main className="flex justify-center  w-full h-full  bg-gradient-to-b from-slate-950 via-slate-900 to-slate-900 text-slate-50   ">
 
-<section className=" w-full h-full md:min-h-screen  max-w-screen-xl flex flex-col items-center ">
-  <h2 className="text-5xl text-cyan-400 font-robotoSerif font-semibold p-4  w-full ">Who I Am</h2>
+                <section className=" w-full h-full md:min-h-screen  max-w-screen-xl flex flex-col items-center ">
+                    <h2 className="text-5xl text-cyan-400 font-robotoSerif font-semibold p-4  w-full ">Who I Am</h2>
 
 
-                    <div className="flex flex-wrap rounded-lg px-4 pb-4 ">
+                    <div className="flex flex-wrap rounded-lg p-4 ">
 
                         <div className=" w-full rounded-lg   h-[100vw] md:h-auto md:w-1/2   rounded-large  relative bg-gradient-to-b from-cyan-800 via-slate-950 to-slate-900 transform transition-transform duration-300 " >
                             <Image
@@ -22,7 +22,7 @@ export default function Who() {
                                 sizes="(max-width: 640px) 50vw, (max-width: 768px) 80vw, 1200px"
                                 className="rounded-full "
                             />
-                        </div>  
+                        </div>
 
                         <div className="p-4 rounded-md md:w-1/2 flex flex-col bg-gradient-to-b from-slate-900 via-slate-950  to-cyan-950 items-center justify-center text-base md:text-lg text-center md:text space-y-4 ">
                             <CodeBrackets />
